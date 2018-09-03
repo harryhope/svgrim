f3b4b3454c8244bb064894fb14ed655ea1897540
Harry Hope <harry.hope@gmail.com>
2018-09-03 16:05:13 -0400

    replace feature

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const fs = require('fs')
 const app = require('commander')
 const svgr = require('@svgr/core').default
@@ -6,27 +7,40 @@ const util = require('util')
 const readdir = util.promisify(fs.readdir)
 const readfile = util.promisify(fs.readFile)
 const rename = filename => filename.replace('.svg', '.js')
-console.log(svgr)
+const componentName = filename => _.upperFirst(_.camelCase(_.trim(filename.replace('.svg', ''))))
+const esc = str =>
+  str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+
 app
   .version('1.0.0')
   .option('-d, --dir [value]', 'A directory of svgs to convert', './')
+  .option('-r, --replace <values>', 'Strings to replace', str => str.split(','))
+  .option('-w, --with <values>', 'Values to replace --replace with', str => str.split(','))
   .parse(process.argv)
 
 const main = async (settings) => {
   try {
     const files = await readdir(settings.dir)
     const svgs = files.filter(file => file.indexOf('.svg') > -1)
-    console.log(svgs)
-    const reads = svgs.map(svg => readfile(settings.dir + svg, 'utf8'))
+    const reads = svgs.map(svg => Promise.all([readfile(settings.dir + svg, 'utf8'), svg]))
     const resolvedFiles = await Promise.all(reads)
-    const writes = resolvedFiles.map(svg => svgr(svg))
+    const writes = resolvedFiles.map(([svg, name]) => svgr(svg, {}, {componentName: componentName(name)}))
     const results = await Promise.all(writes)
-    console.log(results)
+    const transforms = settings.replace && settings.with ? results.map(svg =>
+      settings.replace.reduce((acc, replacee, index) => {
+        const replacement = settings.with[index]
+        console.log(replacee, replacement)
+        return acc.replace(
+          new RegExp(esc(replacee), 'g'),
+          replacement
+        )
+      }, svg)
+    ) : results
+    console.log(transforms)
   } catch (err) {
     console.error('\033[1mError:\033[0m ', err)
     process.exit(1)
   }
-
 }
 
 main(app)
